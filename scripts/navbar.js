5590eafd258b832ceaf3306db1a94bd9fb4b0746
Æscanor <113015001+Aescanor@users.noreply.github.com>
2023-06-23 23:40:48 +0200

    html sass in progress - im tired

---
+++ scripts/navbar.js
@@ -0,0 +1,7 @@
+const navbarToggle = document.querySelector('.navbar-toggle');
+const navbarMenu = document.querySelector('.navbar-menu');
+
+navbarToggle.addEventListener('click', () => {
+  navbarMenu.classList.toggle('show');
+  alert("click")
+});
