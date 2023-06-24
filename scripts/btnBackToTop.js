fae15fa9a42411c9f4c11aa33981739e70c417c4
Æscanor <113015001+Aescanor@users.noreply.github.com>
2023-06-24 12:50:39 +0200

    add html in sections and sass

---
+++ scripts/btnBackToTop.js
@@ -0,0 +1,16 @@
+const backToTopBtn = document.querySelector('.back-to-top-btn');
+
+window.addEventListener('scroll', () => {
+  if (window.scrollY > 200) {
+    backToTopBtn.classList.add('show');
+  } else {
+    backToTopBtn.classList.remove('show');
+  }
+});
+
+backToTopBtn.addEventListener('click', () => {
+  window.scrollTo({
+    top: 0,
+    behavior: 'smooth'
+  });
+});
