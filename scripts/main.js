051549782b98d93eacaef5b13a040e086ce1aeb8
Aescanor <zodiaquestar@gmail.com>
2023-06-26 14:20:34 +0200

    review sass MBF

--- scripts/main.js
+++ scripts/main.js
@@ -81,15 +81,3 @@ document.addEventListener("DOMContentLoaded", () => {
   btnShowFilter.addEventListener("click", () => {
     filter.classList.toggle("showFilter");
   });
-
-
-// window.addEventListener('resize', () => {
-//   const cardContent = document.querySelector('.selection .card-content');
-//   const screenWidth = window.innerWidth;
-
-//   if (screenWidth < 728) {
-//     cardContent.classList.add('truncate');
-//   } else {
-//     cardContent.classList.remove('truncate');
-//   }
-// });
