986fbb2c7013c8e6a3439e396f176891af610e80
Æscanor <113015001+Aescanor@users.noreply.github.com>
2024-06-25 15:06:58 +0200

    merge Develop-in-Ts

--- scripts/navbar.js
+++ scripts/navbar.js
@@ -4,17 +4,3 @@ const btnResponsive = document.querySelector(".btn-responsive");
 btnResponsive.addEventListener("click", () => {
   liste.classList.toggle("show");
 });
-
-
-function adjustNav() {
-  if (window.innerWidth <= 768) {
-    btnResponsive.style.display = "block";
-    liste.classList.remove("show"); 
-  } else {
-    btnResponsive.style.display = "none";
-    liste.classList.remove("show");
-  }
-}
-
-window.addEventListener("resize", adjustNav);
-adjustNav();
